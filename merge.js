b2bb99cee44df0d0ff262c9bfde3ecd75fc10e8e
bl00kman <Leifmhanson@gmail.com>
2024-10-06 11:00:53 -0500

    redoing merge
    using arrow functions to make it smaller and easier

--- merge.js
+++ merge.js
@@ -5,13 +5,5 @@
     });
   }*/
 
-  const filterOutOdds = () => {
-    var nums = Array.prototype.slice.call(arguments);
-  return nums.filter(function(num) {
-    return num % 2 === 0
-});
-}
-   
-   
-    const double = (n) => n * 2;
-console.log (double(3))
+  const filterOutOdds = () => args.filter(v => v % 2 === 0)
+
